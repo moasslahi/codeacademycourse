06d23492fe91b308df1f7031293877f7f31fe666
Mohammed Asslahi <asslahi37@gmail.com>
2020-02-03 12:24:00 +0300

    intro to js

---
+++ JavaScript/IntroToJS.js
@@ -0,0 +1,22 @@
+// Data is printed, or logged, to the console,
+// a panel that displays messages, with console.log().
+console.log("Mo");
+
+// There are 7 fundamental data types in JavaScript:
+// strings, numbers, booleans, null, undefined, symbol, and object.
+
+// The built-in arithmetic operators include +, -, *, /, and %.
+
+// Objects, including instances of data types, can have properties,
+// stored information. The properties are denoted
+// with a . after the name of the object, for example:
+console.log("Hello".length);
+
+// Objects, including instances of data types, can have methods
+// which perform actions. Methods are called by appending the object
+// or instance with a period, the method name, and parentheses. For example:
+console.log("hello".toUpperCase());
+
+// Built-in objects, including Math, are collections of methods and
+// properties that JavaScript provides.
+console.log(Number.isInteger(2012));
