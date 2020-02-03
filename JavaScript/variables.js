20dec7c031ec65d30c87f1efee419c12e577d70f
Mohammed Asslahi <asslahi37@gmail.com>
2020-02-03 22:16:00 +0300

    intro to variables

---
+++ JavaScript/variables.js
@@ -0,0 +1,29 @@
+// Variables hold reusable data in a program
+// and associate it with a name.
+// Variables are stored in memory.
+
+
+// The var keyword is used in pre-ES6 versions of JS.
+
+// let is the preferred way to declare a variable when it can be reassigned, and const is the preferred way to declare a variable with a constant value.
+// Variables that have not been initialized store the primitive data type undefined.
+
+// Mathematical assignment operators make it easy to calculate a new value and assign it to the same variable.
+// The + operator is used to concatenate strings including string values held in variables
+
+
+// In ES6, template literals use backticks ` and ${} to interpolate values into a string.
+// The typeof keyword returns the data type (as a string) of a value.
+
+var 
+let 
+const
+
+let num1 = 6
+num1 +=1;
+num1++;
+
+const name = 'Mo'
+console.log(`My name is ${name}`);
+
+console.log(typeof name);
